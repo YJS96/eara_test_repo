be156136d8a625a1420b9a75510b3fca5b7cc397
임준수 <yimj57027@hotmail.com>
2023-11-06 11:30:23 +0900

    dd

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -28,6 +28,33 @@ export default function SearchBar() {
     setInputValue("");
   }
 
+  const users = [
+    {
+      profileImg: "",
+      nickname: "어쩌라고라고어쩌라고",
+      gru: 25000,
+      isFollow: true,
+    },
+    {
+      profileImg: "",
+      nickname: "어쩌라고",
+      gru: 25000,
+      isFollow: true,
+    },
+    {
+      profileImg: "",
+      nickname: "어쩌",
+      gru: 25000,
+      isFollow: true,
+    },
+    {
+      profileImg: "",
+      nickname: "지구 지킴",
+      gru: 25000,
+      isFollow: true,
+    },
+  ];
+
   return (
     <>
       <SearchBarFrame>
@@ -44,7 +71,15 @@ export default function SearchBar() {
         <CancelButton isFocused={isFocused} onClick={handleCancel}>취소</CancelButton>
       </SearchBarFrame>
       <SearchResultFrame isFocused={isFocused}>
-
+        {users.map((user) => (
+            <UserInfoContainer>
+              <ProfileImg src={user.profileImg} />
+              <TextBox>
+                {user.nickname}
+                <SubText>{user.gru}그루</SubText>
+              </TextBox>
+            </UserInfoContainer>
+          ))}
       </SearchResultFrame>
     </>
   );
@@ -98,7 +133,8 @@ const CancelButton = styled.div<{isFocused : Boolean}>`
 
 const SearchResultFrame = styled.div<{ isFocused: boolean }>`
   position: absolute;
-  width: 100%;
+  left: 0;
+  right: 0;
   margin-top: calc(env(safe-area-inset-top) + 53px);
   height: calc(100% - 76px - 53px);
   z-index: 1;
@@ -108,4 +144,37 @@ const SearchResultFrame = styled.div<{ isFocused: boolean }>`
   opacity: ${props => (props.isFocused ? '1' : '0')};
   transform: translateY(${props => (props.isFocused ? '0' : '20px')});
   transition: visibility 0.3s, opacity 0.3s, transform 0.3s;
+`;
+
+const UserInfoContainer = styled.div`
+  position: relative;
+  left: 0;
+  right: 0;
+  padding: 0 4.44%;
+  display: flex;
+  align-items: center;
+  margin: 20px 0;
+`;
+
+const ProfileImg = styled.img`
+  width: 52px;
+  height: 52px;
+  border-radius: 50%;
+  border: 0.5px solid var(--nav-gray);
+  box-sizing: border-box;
+`;
+
+const TextBox = styled.div`
+  flex-grow: 1;
+  margin-left: 12px;
+  font-size: 15px;
+  font-weight: 400;
+  word-wrap: break-word;
+`;
+
+const SubText = styled.div`
+  margin-top: 5px;
+  font-size: 12px;
+  color: var(--dark-gray);
+  margin-bottom: 3px;
 `;
