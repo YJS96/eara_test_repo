13398ba7c5a7f2ec4488b60a54bb2de03ea7bb54
임준수 <yimj57027@hotmail.com>
2023-11-07 17:57:26 +0900

    D

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
 import Router from './router/Router';
 import './App.css'
+import { useEffect } from 'react';
 
 function App() {
+  const [windowHeight, setWindowHeight] = useState(window.innerHeight);
 
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowHeight(window.innerHeight);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
   return (
     <>
-      <Router></Router>
+      <div id="root" style={{  height: windowHeight }}>
+        <Router ></Router>
+      </div>
     </>
   )
 }
