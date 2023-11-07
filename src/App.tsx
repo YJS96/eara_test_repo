f3a061a23a9ba8460d906c211d83cde64d3c8202
임준수 <yimj57027@hotmail.com>
2023-11-07 17:48:37 +0900

    d

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,13 @@
-import Router from "./router/Router";
-import "./App.css";
+import Router from './router/Router';
+import './App.css'
 
 function App() {
-  function preventDefault(e: any) {
-    e.preventDefault();
-  }
-  // @ts-ignore
-  function disableScroll() {
-    document.body.addEventListener("touchmove", preventDefault, {
-      passive: false,
-    });
-  }
-  // @ts-ignore
-  function enableScroll() {
-    document.body.removeEventListener("touchmove", preventDefault);
-  }
+
   return (
     <>
       <Router></Router>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
