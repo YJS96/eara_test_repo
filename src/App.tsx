53fc5ececfd1171a07289fc4186aae06e731d429
임준수 <yimj57027@hotmail.com>
2023-11-10 10:36:31 +0900

    d

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,11 @@ import Router from './router/Router';
 import './App.css'
 
 function App() {
+  const customVH = window.innerHeight;
+  document.documentElement.style.setProperty("--vh", customVH + "px");
+  window.addEventListener("resize", () => {
+    document.documentElement.style.setProperty("--vh", customVH + "px");
+  });
 
   return (
     <>
