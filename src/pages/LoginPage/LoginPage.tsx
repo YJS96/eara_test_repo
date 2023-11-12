0c8abf793478d59e23353b21d2131e6095316139
임준수 <yimj57027@hotmail.com>
2023-11-12 14:40:58 +0900

    d

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,12 +1,41 @@
 // import React from 'react'
+import { useEffect, useState } from "react";
 import styled from "styled-components";
 import MainFrame from "../../components/MainFrame/MainFrame";
 import { ReactComponent as LoginBackgroundSVG } from "../../assets/icons/login-background.svg";
+import { ReactComponent as Download } from "../../assets/icons/download-icon.svg";
 
 export default function LoginPage() {
   const helpURL = "http://pf.kakao.com/_xbxhxgsG";
   const netZeroURL =
     "https://cpoint.or.kr/netzero/member/nv_memberRegistStep1.do";
+
+  const [supportsPWA, setSupportsPWA] = useState(false);
+  const [promptInstall, setPromptInstall] = useState(null);
+
+  useEffect(() => {
+    const handler = (e: any) => {
+      e.preventDefault();
+      setSupportsPWA(true);
+      setPromptInstall(e);
+    };
+    window.addEventListener("beforeinstallprompt", handler);
+
+    return () => window.removeEventListener("transitionend", handler);
+  }, []);
+
+  const installApp = (event: any) => {
+    event.preventDefault();
+    if (!promptInstall) {
+      return;
+    }
+    // @ts-ignore
+    promptInstall.prompt();
+
+    if (!supportsPWA) {
+      return null;
+    }
+  };
 
   return (
     <>
@@ -14,21 +43,24 @@ export default function LoginPage() {
         <BackgroundFrame>
           <LoginBackground />
         </BackgroundFrame>
-        <EARA>
-          <EARABold>어 - 라?</EARABold>
-          <br />
-          어느 날 갑자기
-          <br />
-          지구가 당신을
-          <br />
-          고소한다면?
-        </EARA>
-        {/* <LogoImage src="/images/logo-nobackground.png"/> */}
         <ButtonsFrame>
+          <EARA>
+            <EARABold>어 - 라?</EARABold>
+            <br />
+            어느 날 갑자기
+            <br />
+            지구가 당신을
+            <br />
+            고소한다면?
+          </EARA>
           <KakaoButton>
             카카오로 시작하기
-            <KakaoLogo src="images/kakao-logo.png" />
+            <ButtonLogo src="images/kakao-logo.png" />
           </KakaoButton>
+          <InstallButton onClick={installApp}>
+            어플리케이션 설치
+            <DownloadIcon />
+          </InstallButton>
           <HelpButtonsFrame>
             <HelpButton
               onClick={() => {
@@ -45,11 +77,6 @@ export default function LoginPage() {
               탄소중립포인트제도
             </HelpButton>
           </HelpButtonsFrame>
-          <Terms>
-            회원가입 시 어라의 개인정보 처리방침 및 이용약관에
-            <br />
-            동의하는 것으로 간주합니다
-          </Terms>
         </ButtonsFrame>
       </MainFrame>
     </>
@@ -72,7 +99,7 @@ const LoginBackground = styled(LoginBackgroundSVG)`
 `;
 
 const EARA = styled.div`
-  margin-top: 40px;
+  margin-bottom: 56px;
   font-size: 1.625em;
   font-weight: 400;
   line-height: 1.4em;
@@ -94,11 +121,11 @@ const EARABold = styled.span`
 
 const ButtonsFrame = styled.div`
   position: absolute;
-  height: 220px;
   background-color: var(--white);
   left: 0;
   right: 0;
   bottom: 0;
+  max-height: 55%;
 `;
 
 const KakaoButton = styled.div`
@@ -115,13 +142,24 @@ const KakaoButton = styled.div`
   font-weight: 550;
   font-size: 14px;
   color: var(--kakao-black);
-  margin-top: 24px;
 `;
 
-const KakaoLogo = styled.img`
+const InstallButton = styled(KakaoButton)`
+  background-color: var(--black);
+  color: var(--white);
+  font-weight: 450;
+  margin-top: 16px;
+`;
+
+const ButtonLogo = styled.img`
   position: absolute;
   left: 18px;
   width: 28px;
+`;
+
+const DownloadIcon = styled(Download)`
+  position: absolute;
+  left: 18px;
 `;
 
 const HelpButtonsFrame = styled.div`
@@ -133,19 +171,11 @@ const HelpButtonsFrame = styled.div`
   display: flex;
   justify-content: space-evenly;
   align-items: center;
+  margin-bottom: 32px;
 `;
 
 const HelpButton = styled.div`
   font-size: 13px;
   color: var(--dark-gray);
   font-weight: 500;
-`;
-
-const Terms = styled.div`
-  left: 0;
-  right: 0;
-  margin-top: 14px;
-  text-align: center;
-  font-size: 11.5px;
-  font-weight: 300;
 `;
