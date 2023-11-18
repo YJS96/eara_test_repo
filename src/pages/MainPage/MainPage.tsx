43e8f06c854bcddb839fdc5ad39772692b54647c
임준수 <yimj57027@hotmail.com>
2023-11-19 02:39:54 +0900

    published

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,268 +1,80 @@
 import styled from "styled-components";
-import NavBar from "../../components/NavBar/NavBar";
-import MainFrame from "../../components/MainFrame/MainFrame";
-import { ModalFrame } from "../../style/ModalFrame";
-import ProgressBar from "../../components/ProgressBar/ProgressBar";
-import { ShortButton } from "../../style";
-import { useNavigate } from "react-router-dom";
-
-import { ReactComponent as Notification } from "../../assets/icons/notification-icon.svg";
 
 export default function MainPage() {
-  const navigate = useNavigate();
 
-  const toLogin = () => {
-    navigate("/login")
+  const toEara = () => {
+    location.href="https://www.ea-ra.com/login"
   }
-  
-  const toSignup = () => {
-    navigate("/signup")
-  }
-  
-  const toWelcome = () => {
-    navigate("/welcome")
-  }
-  
-  const toNotification = () => {
-    navigate("/notice");
-  };
-
-  const toMonthCal = () => {
-    navigate("/calendar");
-  };
-
-  const toAct = () => {
-    navigate("/act");
-  };
-
-  function getCurrentWeek() {
-    const day = new Date();
-    const sunday = day.getTime() - 86400000 * day.getDay();
-
-    day.setTime(sunday);
-
-    const result = [day.toISOString().slice(0, 10)];
-
-    for (let i = 1; i < 7; i++) {
-      day.setTime(day.getTime() + 86400000);
-      result.push(day.toISOString().slice(0, 10));
-    }
-
-    return result;
-  }
-
-  const currentWeek = getCurrentWeek();
-
-  const onlyDay = currentWeek.map((date) => {
-    const lastTwoChars = date.slice(-2);
-    if (lastTwoChars.startsWith("0")) {
-      return lastTwoChars.substring(1);
-    }
-    return lastTwoChars;
-  });
-
-  var progress = 100;
-  var greenInit = 2400000;
-
-  const groo_saving_list = [
-    {
-        "date": "2023-11-5",
-        "proof_count": 1
-    },
-    {
-        "date": "2023-11-6",
-        "proof_count": 0
-    },
-    {
-        "date": "2023-11-7",
-        "proof_count": 2
-    },
-    {
-        "date": "2023-11-8",
-        "proof_count": 0
-    },
-    {
-        "date": "2023-11-9",
-        "proof_count": 4
-    },
-    {
-        "date": "2023-11-10",
-        "proof_count": 1
-    },
-    {
-        "date": "2023-11-11",
-        "proof_count": 2
-    }
-]
-
-  const getCountColor = (count: number): string => {
-    if (count === 0) {
-      return "var(--white)";
-    } else if (count <= 1) {
-      return "var(--third)";
-    } else if (count <= 2) {
-      return "var(--secondary)";
-    } else {
-      return "var(--primary)";
-    }
-  };
 
   return (
     <>
-      <MainFrame headbar="no" navbar="yes" bgcolor="third" marginsize="no">
-    <br/><br/><br/><br/><br/><br/><br/>
-      <div onClick={toLogin}>로그인 페이지</div><br/><br/>
-      <div onClick={toSignup}>회원가입 페이지</div><br/><br/>
-      <div onClick={toWelcome}>테스트 페이지</div><br/><br/>
-        <NotificationIcon onClick={toNotification} />
-        <HomeFrame>
-          <ShowDate>10월 23일 기준</ShowDate>
-          <NicknameLine>
-            <Bold>환경구해</Bold>님의 남은 빚
-          </NicknameLine>
-          <GreenLeft>
-            <Bold>1,242,600</Bold>그린
-          </GreenLeft>
-
-          <ProgressBar progress={progress} greeninit={greenInit} />
-
-          <SummaryText>주간 활동 요약</SummaryText>
-
-          <WeekdayFrame>
-            <WeekNameFrame>
-              <WeekName>일</WeekName>
-              <WeekName>월</WeekName>
-              <WeekName>화</WeekName>
-              <WeekName>수</WeekName>
-              <WeekName>목</WeekName>
-              <WeekName>금</WeekName>
-              <WeekName>토</WeekName>
-            </WeekNameFrame>
-            {onlyDay.map((day, index) => (
-              <OneDay>
-                <DayNumber>{day}</DayNumber>
-                <DayProgress count={getCountColor(groo_saving_list[index].proof_count)} />
-              </OneDay>
-            ))}
-          </WeekdayFrame>
-
-          <ButtonsFrame>
-            <ShortButton
-              background="var(--third)"
-              color="var(--primary)"
-              onClick={toAct}
-            >
-              남은 빚 갚기
-            </ShortButton>
-            <ShortButton onClick={toMonthCal}>월별 내역</ShortButton>
-          </ButtonsFrame>
-        </HomeFrame>
+      <MainFrame>
+        <LogoFrame onClick={toEara}>
+          <Logo src="/images/logo-nobackground.png" />
+        </LogoFrame>
+        <AppName>
+          어라
+        </AppName>
+        <Introduce>
+          어라가 정식 배포 되었습니다
+        </Introduce>
+        <Button onClick={toEara}>사용해보기</Button>
       </MainFrame>
-      <NavBar />
     </>
   );
 }
 
-const NotificationIcon = styled(Notification)`
-  position: absolute;
-  right: 5.56%;
-  top: max(calc(env(safe-area-inset-top) + 4px), 28px);
-  filter: drop-shadow(2px 2px 6px rgba(0, 0, 0, 0.12));
-`;
-
-const HomeFrame = styled(ModalFrame)`
-  padding: 0px 5.56%;
-  max-height: 56.4%;
-  font-weight: 400;
-  overflow-y: scroll;
-`;
-
-const ShowDate = styled.div`
-  margin-top: calc(4px + 5.56%);
-  font-size: 12px;
-  color: var(--dark-gray);
-`;
-
-const NicknameLine = styled.div`
-  margin-top: 3px;
-  font-size: 18px;
-`;
-
-const Bold = styled.span`
-  font-weight: 600;
-`;
-
-const GreenLeft = styled.div`
-  margin-top: 14px;
-  font-size: 28px;
-  margin-bottom: 8px;
-`;
-
-const SummaryText = styled.div`
-  margin-top: 24px;
-  font-size: 14.5px;
-  font-weight: 550;
-`;
-
-const WeekdayFrame = styled.div`
-  position: relative;
-  width: 105.56%;
-  margin-left: -2.78%;
-  height: 84px;
-  margin-top: 8px;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  flex-wrap: wrap;
-`;
-
-const WeekNameFrame = styled.div`
+const MainFrame = styled.div`
   position: relative;
   width: 100%;
+  height: 100%;
   display: flex;
-  justify-content: space-between;
-  font-size: 12px;
-  margin-top: 14px;
-  margin-bottom: 12px;
-`;
-
-const WeekName = styled.div`
-  position: relative;
-  width: calc(100% / 7);
-  text-align: center;
-`;
-
-const OneDay = styled.div`
-  position: relative;
-  width: 14.2%;
-  height: 90%;
-  display: flex;
-  flex-wrap: wrap;
   justify-content: center;
   align-items: center;
-`;
+  flex-direction: column;
+`
 
-const DayNumber = styled.div`
+const LogoFrame = styled.div`
   position: relative;
-  width: 100%;
-  text-align: center;
-  font-size: 13.5px;
-  font-weight: 500;
-`;
-
-const DayProgress = styled.div<{ count: string }>`
-  width: 8px;
-  height: 8px;
-  border-radius: 2px;
-  margin-top: 4px;
-  background-color: ${(props) => props.count};
-`;
-
-const ButtonsFrame = styled.div`
-  position: relative;
-  width: 100%;
-  margin-top: 56px;
+  width: 100px;
+  height: 100px;
+  border-radius: 20px;
+  background-color: var(--white);
+  box-shadow: 2px 2px 8px rgba(0,0,0, 0.08);
   display: flex;
-  justify-content: space-between;
-  margin-bottom: 24px;
-`;
+  justify-content: center;
+  align-items: center;
+`
+
+const Logo = styled.img`
+  position: relative;
+  width: 84%;
+  height: 84%;
+`
+
+const AppName = styled.div`
+  margin-top: 16px;
+  font-size: 18px;
+  font-weight: 400;
+`
+
+const Introduce = styled.div`
+  margin-top: 48px;
+  margin-bottom: 56px;
+  font-size: 15px;
+`
+
+const Button = styled.div`
+  position: absolute;
+  bottom: 60px;
+  width: 320px;
+  height: 44px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: var(--white);
+  background-color: var(--primary);
+  border-radius: 10px;
+  font-weight: 400;
+  font-size: 15px;
+`
